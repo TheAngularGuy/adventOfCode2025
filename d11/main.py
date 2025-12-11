59cd2ea7fe7607a7511cb0f99cec4fbdba476294
mustapha <aouas.m@gmail.com>
2025-12-11 21:07:16 +0100

    aoc2025 d11 part2

--- d11/main.py
+++ d11/main.py
@@ -1,4 +1,3 @@
-from collections import deque
 from functools import cache
 
 with open("input.txt", "r") as file:
@@ -12,37 +11,27 @@ for row in input_data:
     list_connections = rest.strip().split(' ')
     dictionary[name.strip()] = list_connections
 
+
 @cache
-def solve_for(start, finish):
-    q = deque()
-    q.append((start, []))
+def solve_from_to(curent, finish):
+    if curent == finish:
+        return 1
 
-    paths = []
-    while len(q) > 0:
-        current_name, history = q.popleft()
-        if current_name == finish:
-            paths.append(history + [current_name])
-            continue
-
-        if not current_name in dictionary:
-            continue
-
-        current_connections = dictionary[current_name]
-        for connection in current_connections:
-            if connection not in history:
-                q.append((connection, history + [current_name]))
-
-    return len(paths)
+    result = 0
+    if curent in dictionary:
+        for connection in dictionary[curent]:
+            result += solve_from_to(connection, finish)
+    return result
 
 
 def part1():
-    print("Part 1:", solve_for("you", "out"))
+    print("Part 1:", solve_from_to("you", "out"))
 
 
 def part2():
     print("Part 2:",
-          solve_for("svr", "dac") * solve_for("dac", "fft") * solve_for("fft", "out") +
-          solve_for("svr", "fft") * solve_for("fft", "dac") * solve_for("dac", "out")
+          solve_from_to("svr", "dac") * solve_from_to("dac", "fft") * solve_from_to("fft", "out") +
+          solve_from_to("svr", "fft") * solve_from_to("fft", "dac") * solve_from_to("dac", "out")
           )
 
 
