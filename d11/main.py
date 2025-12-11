92f3ab0a892b48fb17f96425d00c54d3c0710169
mustapha <aouas.m@gmail.com>
2025-12-11 19:41:41 +0100

    aoc2025 d11 part1

--- d11/main.py
+++ d11/main.py
@@ -1,5 +1,4 @@
-from itertools import combinations
-from z3 import *
+from collections import deque
 
 with open("input.txt", "r") as file:
     content = file.read()
@@ -8,8 +7,27 @@ input_data: list[str] = list(filter(lambda el: len(el), content.split('\n')))
 
 
 def part1():
-    total = 0
-    print("Part 1:", total)
+    dictionary = dict()
 
+    for row in input_data:
+        name, rest = row.split(':')
+        list_connections = rest.strip().split(' ')
+        dictionary[name.strip()] = list_connections
 
+    q = deque()
+    q.append(('you', []))
+
+    paths = []
+    while len(q) > 0:
+        current_name, history = q.popleft()
+        if current_name == 'out':
+            paths.append(history + [current_name])
+            continue
+
+        current_connections = dictionary[current_name]
+        for connection in current_connections:
+            if connection not in history:
+                q.append((connection, history + [current_name]))
+
+    print("Part 1:", len(paths))
 part1()
